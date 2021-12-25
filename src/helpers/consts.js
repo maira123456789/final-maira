09ff3ddd4ee5d681c6abe4cc506f01f2fd3f5941
M Uzakbekov <muzakbekov@MacBook-Air-M.local>
2021-12-25 20:51:08 +0600

    last commit

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,7 +1,7 @@
-export const PRODUCTS_API = "https://retro-cars.herokuapp.com/apiproducts";
+export const PRODUCTS_API = "http://localhost:8000/products";
 
-export const BRANDS_API = "https://retro-cars.herokuapp.com/apibrands";
+export const BRANDS_API = "http://localhost:8000/brands";
 
-export const COMMENTS_API = "https://retro-cars.herokuapp.com/apicomments";
+export const COMMENTS_API = "http://localhost:8000/comments";
 
-export const LIKES_API = "https://retro-cars.herokuapp.com/apilikes";
+export const LIKES_API = "http://localhost:8000/likes";
